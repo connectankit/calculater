cd741b163edad176e05f9b8beed6a8f2faa9a752
Ankit Kumar <ankitkumarasus@gmail.com>
2021-10-02 21:57:10 +0530

    clear button logic apply

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 let screen = document.querySelector("input");
 var buttons = document.querySelectorAll('button');
-let screenValue = "";
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click",function(){
+    buttons[i].addEventListener("click", function () {
         buttonInnerHtml = buttons[i].innerHTML;
-        console.log(buttonInnerHtml)
-       if (buttonInnerHtml == "AC") {
-           screenValue = "";
-           screen.value = screenValue;
+        if (buttonInnerHtml == "AC") {
+            screen.value = '';
 
-       }
-       else if(buttonInnerHtml == "X"){
-           buttonInnerHtml = "*";
-       screenValue += buttonInnerHtml;
-       screen.value = screenValue;
-    }
-    else if (buttonInnerHtml == "="){
-        screen.value = eval(screenValue);
-        
+        }
+        else if (buttonInnerHtml == "X") {
+            buttonInnerHtml = "*";
+            screen.value += buttonInnerHtml;
+            //  = screenValue;
+        }
+        else if (buttonInnerHtml == "=") {
+            screen.value = eval(screen.value);
 
-    }
-    else if (buttonInnerHtml == "C"){
-    screenValue = screenValue - screenValue
-    screen.value = screenValue;
-    console.log(screenValue)
-    }
-       else{
-           screenValue += buttonInnerHtml;
-           screen.value = screenValue;
-       } 
-       
+
+        }
+        else if (buttonInnerHtml == "C") {
+            let remove = screen.value;
+            let removeLast = remove.slice(0,-1)
+            console.log(removeLast);
+            screen.value = removeLast;
+            console.log(screen.value)
+            
+            
+        }
+        else {
+            screen.value += buttonInnerHtml;
+            //  = screenValue;
+        }
+
     })
-    
+
 }
 
